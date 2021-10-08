32631930313da679bfe820a5e26b88979fca37f1
Chitra <jbhurtun@hotmail.com>
2021-10-08 16:52:34 +0400

    Master - Design of RegisteredUserDashboard

---
+++ app/src/main/java/com/example/mauritiusdiseasefocus/RegisteredUserDashboard.java
@@ -0,0 +1,14 @@
+package com.example.mauritiusdiseasefocus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisteredUserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered_user_dashboard);
+    }
+}
