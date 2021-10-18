0549981c5510f4be13d0b423581dae0dd9818d8c
Chitra <jbhurtun@hotmail.com>
2021-10-18 11:12:52 +0400

    Chitra - Sending email working fine - for sms code has been written but need to test when app is installed in mobile

---
+++ app/src/main/java/com/example/mauritiusdiseasefocus/Utils.java
@@ -0,0 +1,7 @@
+package com.example.mauritiusdiseasefocus;
+
+public class Utils {
+
+    public static final String email = "chitrabhurtunseegoola202006571@gmail.com";
+    public static final String password = "Robinsharma08***";
+}
